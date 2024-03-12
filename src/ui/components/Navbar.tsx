4e4250c77de8e23747562fd582f067d8648a6aed
vicsan123 <86122692+vicsan123@users.noreply.github.com>
2024-03-12 19:03:25 +0100

    Update Navbar.tsx

--- src/ui/components/Navbar.tsx
+++ src/ui/components/Navbar.tsx
@@ -10,7 +10,7 @@ function Navbar() {
   // Función para alternar la visibilidad del menú desplegable
   const toggleDropdown = () => setIsDropdownOpen(!isDropdownOpen);
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <nav className="bg-gray-900 border-gray-200">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <Link
           to="/"
