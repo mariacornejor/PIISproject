d9c94d92bb6c051d16982c555f98de80016c5542
vicsan123 <86122692+vicsan123@users.noreply.github.com>
2024-03-19 18:27:22 +0100

    Create Logrositem.tsx

---
+++ src/ui/components/Logrositem.tsx
@@ -0,0 +1,23 @@
+type Props = {
+  src: string;
+  nombre: string;
+  descripcion: string;
+};
+
+function Logrositem(props: Props) {
+  return (
+    <div className="bg-leaderboard text-white p-3 rounded-lg shadow-md flex items-center space-x-4 m-4 max-w-96">
+      <img
+        src={props.src}
+        alt={props.nombre}
+        className="w-20 h-20 rounded-2xl object-cover"
+      />
+      <div>
+        <div className="text-lg font-bold">{props.nombre}</div>
+        <div className="text-sm">{props.descripcion}</div>
+      </div>
+    </div>
+  );
+}
+
+export default Logrositem;
