954428afdaddc85a771ae6fdc1f2b63306237af2
Your Name <you@example.com>
2024-03-20 01:16:51 +0100

    Login pero NO

--- src/pages/components/Login.tsx
+++ src/pages/components/Login.tsx
@@ -4,16 +4,13 @@ import icon from "../../assets/images/icono.png";
 
 function Login() {
     const [username, setUsername] = useState('');
-    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
     const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         console.log('Username:', username);
-        console.log('Email:', email);
         console.log('Password:', password);
         setUsername('');
-        setEmail('');
         setPassword('');
     };
 
@@ -23,23 +20,15 @@ function Login() {
                 <img src={icon} style={{ width: '100px', height: '90px' }} alt="Icono" />
             </Link>
 
-            <h2 style={{ color: 'white', fontSize: '2.3rem', marginBottom: '1rem', fontFamily: 'Montserrat', fontWeight: 'bold' }}>Registrar nuevo usuario</h2>
-            <div style={{ marginTop: '1rem', width: '100%', maxWidth: '500px', textAlign: 'center'}}>
-                <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', gap: '1rem', fontFamily: 'Montserrat'}}>
+            <h2 style={{ color: 'white', fontSize: '2.3rem', marginBottom: '1rem', fontFamily: 'Montserrat', fontWeight: 'bold' }}>Inicia Sesion</h2>
+            <div style={{ marginTop: '1rem', width: '100%', maxWidth: '500px', textAlign: 'center' }}>
+                <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column', gap: '1rem', fontFamily: 'Montserrat' }}>
                     <input
                         type="text"
                         id="username"
                         placeholder="Nombre de usuario"
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
-                        style={{ width: '100%', padding: '1rem', backgroundColor: '#333', border: 'none', color: 'white', borderRadius: '15px' }}
-                    />
-                    <input
-                        type="email"
-                        id="email"
-                        placeholder="Correo electrónico"
-                        value={email}
-                        onChange={(e) => setEmail(e.target.value)}
                         style={{ width: '100%', padding: '1rem', backgroundColor: '#333', border: 'none', color: 'white', borderRadius: '15px' }}
                     />
                     <input
@@ -52,32 +41,41 @@ function Login() {
                     />
                 </form>
             </div>
-            
+
             <Link to="/inicio" style={{ maxWidth: '500px', alignSelf: 'center', cursor: 'pointer', textDecoration: 'none', display: 'flex', justifyContent: 'center' }}>
-    <div style={{ marginTop: '1rem' }}>
-        <button
-            type="submit"
-            style={{
-                width: '200%', // Cambia el ancho del botón según tus necesidades
-                margin: 'auto', // Centra el botón horizontalmente
-                padding: '1rem',
-                backgroundColor: '#E50914',
-                color: 'white',
-                borderRadius: '15px',
-                cursor: 'pointer',
-                fontFamily: 'Montserrat',
-                fontSize: '1rem',
-                fontWeight: 'bold'
-            }}
-        >
-            Crear Cuenta
-        </button>
-    </div>
-</Link>
+                <div style={{ marginTop: '1rem' }}>
+                    <button
+                        type="submit"
+                        style={{
+                            width: '200%', // Cambia el ancho del botón según tus necesidades
+                            margin: 'auto', // Centra el botón horizontalmente
+                            padding: '1rem',
+                            backgroundColor: '#E50914',
+                            color: 'white',
+                            borderRadius: '15px',
+                            cursor: 'pointer',
+                            fontFamily: 'Montserrat',
+                            fontSize: '1rem',
+                            fontWeight: 'bold'
+                        }}
+                    >
+                        Iniciar Sesion
+                    </button>
+                </div>
+
+            </Link>
 
 
+            <div className='flex bottom-[20px] right-[20px] relative'>
+                <button type="submit" className=' p-[1rem] bg-[#E50914] rounded-[15px] cursor-pointer font-bold text-[1rem] font-["Montserrat"] absolute'>
+                    Administrador
+                </button>
+                <button type="submit" className=' p-[1rem] bg-[#E50914] rounded-[15px] cursor-pointer font-bold text-[1rem] font-["Montserrat"] absolute'>
+                    Desarrollador
+                </button>
+            </div>
+        </div>
 
-        </div> 
     );
 }
 
