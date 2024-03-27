836dd987839a022395616f2806ded8a54b59ebe5
diegoovh <dverdu02@alu.ucam.edu>
2024-03-27 19:39:11 +0100

    ForosActu

--- src/pages/components/Foros.tsx
+++ src/pages/components/Foros.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Navbar from "../../ui/components/Navbar";
 import banner from "../../assets/images/banner.png";
 import Secondbar from "../../ui/components/Secondbar";
@@ -11,91 +12,87 @@ import postImage3 from "../../assets/images/bugforo.webp";
 import "../../ui/css/Foros.css"; 
 
 function Foros() {
- return (
-   <div>
-     <Navbar />
-     <img className="banner" src={banner} alt="Banner de la página de foros" />
-     <div className="bg-background body-font relative justify-center">
-       <Secondbar />
-       <div className="forum-container"> 
-         <div className="forum">
-           <div className="user-info">
-             <img className="user-avatar" src={userAvatar1} alt="Avatar del usuario" />
-             <p className="username">EpicGamer99</p>
-             <p className="timestamp">Hace 5 minutos</p>
-           </div>
-           <p className="message">¡Acabo de alcanzar el nivel 50 en el último juego de rol que he estado jugando! ¿Alguien más ha llegado tan lejos?</p>
-         </div>
-         <img className="forum-image" src={postImage1} alt="Imagen del foro" />
-         <div className="spacer"></div>
-         <div className="forum">
-           <div className="user-info">
-             <img className="user-avatar" src={userAvatar2} alt="Avatar del usuario" />
-             <p className="username">SpeedRunnerX</p>
-             <p className="timestamp">Hace 1 hora</p>
-           </div>
-           <p className="message">He encontrado un bug en el último parche de actualización del juego de disparos. ¿Alguien más ha experimentado esto?</p>
-         </div>
-         <img className="forum-image" src={postImage3} alt="Imagen del foro" />
-         <div className="spacer"></div>
-         <div className="forum">
-           <div className="user-info">
-             <img className="user-avatar" src={userAvatar3} alt="Avatar del usuario" />
-             <p className="username">QuestMaster</p>
-             <p className="timestamp">Hace 3 horas</p>
-           </div>
-           <p className="message">¡Estoy buscando nuevos amigos para jugar en línea! Si te gusta jugar en equipo, agrégame como amigo y juguemos juntos.</p>
-         </div>
-         <div className="spacer"></div>
-         <div className="forum">
-           <div className="user-info">
-             <img className="user-avatar" src={userAvatar4} alt="Avatar del usuario" />
-             <p className="username">RetroGamer87</p>
-             <p className="timestamp">Hace 5 horas</p>
-           </div>
-           <p className="message">Recientemente descubrí un juego retro increíble. Es una joya oculta que merece más atención. ¡Deberías probarlo si te gustan los clásicos!</p>
-         </div>
-         <div className="spacer"></div>
-         <div className="forum">
-           <div className="user-info">
-             <img className="user-avatar" src={userAvatar1} alt="Avatar del usuario" />
-             <p className="username">EpicGamer99</p>
-             <p className="timestamp">Hace 1 día</p>
-           </div>
-           <p className="message">He estado debatiendo con algunos amigos sobre cuál es el mejor personaje en el último juego de lucha. ¿Cuál es tu opinión?</p>
-         </div>
-         <div className="spacer"></div>
-         <div className="forum">
-           <div className="user-info">
-             <img className="user-avatar" src={userAvatar2} alt="Avatar del usuario" />
-             <p className="username">SpeedRunnerX</p>
-             <p className="timestamp">Hace 2 días</p>
-           </div>
-           <p className="message">¿Qué os parece la jugada que hize en mi partida de ajedrez de ayer?</p>
-         </div>
-         <img className="forum-image" src={postImage2} alt="Imagen del foro" />
-         <div className="spacer"></div>
-         <div className="forum">
-           <div className="user-info">
-             <img className="user-avatar" src={userAvatar3} alt="Avatar del usuario" />
-             <p className="username">QuestMaster</p>
-             <p className="timestamp">Hace 3 días</p>
-           </div>
-           <p className="message">¡Acabo de completar una misión épica en mi juego de aventuras favorito! No puedo esperar para ver qué sucede después.</p>
-         </div>
-         <div className="spacer"></div>
-         <div className="forum">
-           <div className="user-info">
-             <img className="user-avatar" src={userAvatar4} alt="Avatar del usuario" />
-             <p className="username">RetroGamer87</p>
-             <p className="timestamp">Hace 4 días</p>
-           </div>
-           <p className="message">He estado jugando un juego indie recientemente y estoy impresionado por el arte pixelado. ¿Alguien más aprecia el arte retro en los juegos?</p>
-         </div>
-       </div>
-     </div>
-   </div>
- );
+  const [posts, setPosts] = useState([
+    {
+      userAvatar: userAvatar1,
+      username: "EpicGamer99",
+      timestamp: "Hace 5 minutos",
+      message: "¡Acabo de alcanzar el nivel 50 en el último juego de rol que he estado jugando! ¿Alguien más ha llegado tan lejos?",
+      image: postImage1,
+    },
+    {
+      userAvatar: userAvatar2,
+      username: "SpeedRunnerX",
+      timestamp: "Hace 1 hora",
+      message: "He encontrado un bug en el último parche de actualización del juego de disparos. ¿Alguien más ha experimentado esto?",
+      image: postImage3,
+    },
+    {
+      userAvatar: userAvatar3,
+      username: "QuestMaster",
+      timestamp: "Hace 3 horas",
+      message: "¡Estoy buscando nuevos amigos para jugar en línea! Si te gusta jugar en equipo, agrégame como amigo y juguemos juntos.",
+    },
+    {
+      userAvatar: userAvatar4,
+      username: "RetroGamer87",
+      timestamp: "Hace 5 horas",
+      message: "Recientemente descubrí un juego retro increíble. Es una joya oculta que merece más atención. ¡Deberías probarlo si te gustan los clásicos!",
+    },
+    {
+      userAvatar: userAvatar1,
+      username: "EpicGamer99",
+      timestamp: "Hace 1 día",
+      message: "He estado debatiendo con algunos amigos sobre cuál es el mejor personaje en el último juego de lucha. ¿Cuál es tu opinión?",
+    },
+    {
+      userAvatar: userAvatar2,
+      username: "SpeedRunnerX",
+      timestamp: "Hace 2 días",
+      message: "¿Qué os parece la jugada que hize en mi partida de ajedrez de ayer?",
+      image: postImage2,
+    },
+    {
+      userAvatar: userAvatar3,
+      username: "QuestMaster",
+      timestamp: "Hace 3 días",
+      message: "¡Acabo de completar una misión épica en mi juego de aventuras favorito! No puedo esperar para ver qué sucede después.",
+    },
+    {
+      userAvatar: userAvatar4,
+      username: "RetroGamer87",
+      timestamp: "Hace 4 días",
+      message: "He estado jugando un juego indie recientemente y estoy impresionado por el arte pixelado. ¿Alguien más aprecia el arte retro en los juegos?",
+    },
+  ]);
+
+  return (
+    <div>
+      <Navbar />
+      <div className="extra-space"></div> 
+      <img className="banner" src={banner} alt="Banner de la página de foros" />
+      <div className="bg-background body-font relative justify-center">
+        <Secondbar />
+        <div className="forum-container">
+          {posts.map((post) => (
+            <div key={post.username} className="post">
+              <div className="user-info">
+                <img className="user-avatar" src={post.userAvatar} alt="Avatar del usuario" />
+                <div className="username-timestamp">
+                  <p className="username">{post.username}</p>
+                  <div className="timestamp">{post.timestamp}</div>
+                </div>
+              </div>
+              <div className="extra-space-user"></div> 
+              <div className="post-content">{post.message}</div>
+              {post.image && <img className="forum-image" src={post.image} alt="Imagen del foro" />}
+              <div className="separator-short"></div> 
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Foros;
