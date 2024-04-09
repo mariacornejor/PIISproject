5f3b1f9b5cc139047e09796451429a49b333fbf5
SinnonW <116178190+SinnonW@users.noreply.github.com>
2024-04-09 11:01:08 +0200

    hahaEditMejorado

--- src/pages/components/EditProfile.tsx
+++ src/pages/components/EditProfile.tsx
@@ -1,9 +1,56 @@
-import "react-multi-carousel/lib/styles.css";
+import React, { useState, useEffect } from "react";
 import Navbar from "../../ui/components/Navbar";
 import Input from "../../ui/components/Input";
 import fotoPerfil from "../../assets/images/perroHuman.webp";
 
 function EditProfile() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [profileImage, setProfileImage] = useState("");
+
+  useEffect(() => {
+    obtenerDatosPerfil();
+  }, []);
+
+  async function obtenerDatosPerfil() {
+    try {
+      // Realizar una solicitud al backend para obtener los datos del perfil del usuario
+      const response = await fetch("http://localhost:3000/api/profile", {
+        method: "GET",
+        credentials: "include",
+      });
+      const data = await response.json();
+      if (response.ok) {
+        setUsername(data.username);
+        setEmail(data.email);
+      } else {
+        throw new Error(
+          data.message || "Error al obtener los datos del perfil del usuario"
+        );
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  function handleImageChange(event) {
+    const selectedImage = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function (e) {
+      setProfileImage(e.target.result);
+    };
+
+    reader.readAsDataURL(selectedImage);
+  }
+
+  if (loading) {
+    return <div>Cargando...</div>;
+  }
+
   return (
     <div className="background bg-background pb-36">
       <Navbar />
@@ -15,15 +62,17 @@ function EditProfile() {
           </h1>
           <div className="px-12 flex max-w-96 flex-col gap-2 text-white">
             <form action="" method="post">
-              <Input titulo="Username" valorPredefinido="Coco loco"></Input>
-              <Input
-                titulo="Email"
-                valorPredefinido="cocoloco@gmail.com"
-              ></Input>
-              <Input
-                titulo="Fecha de Nacimiento"
-                valorPredefinido="23/05/1995"
-              ></Input>
+              <Input titulo="Username" valorPredefinido={username}></Input>
+              <Input titulo="Email" valorPredefinido={email}></Input>
+              <label htmlFor="profileImage" className="text-white">
+                Cambiar imagen de perfil
+              </label>
+              <input
+                type="file"
+                id="profileImage"
+                accept="image/*"
+                onChange={handleImageChange}
+              />
               <button className="ml-0 mt-10 top-12 left-13 right-10 z-20 text-sm rounded border-2 border-white bg-white py-1 text-center font-mono font-black uppercase text-neutral-800 backdrop-blur transition-colors hover:bg-white/30 hover:text-white">
                 Aceptar
               </button>
@@ -39,10 +88,22 @@ function EditProfile() {
         {/** Parte de la imagen */}
         <div className="relative py-10 m-3 mt-10 mr-8 rounded-lg items-center flex justify-center">
           <h3 className="px-12 h-8 text-3xl absolute top-0 left-0 right-0 text-white text-center py-2 text-gray-900 font-semibold">
-            COCO LOCO
+            {username}
           </h3>
           <div className="w-96 h-96 overflow-hidden rounded-xl border-2 border-gray-300 shadow-lg relative">
-            <img src={fotoPerfil} alt="" className="w-full h-auto" />
+            {profileImage ? (
+              <img
+                src={profileImage}
+                alt="Imagen de perfil"
+                className="w-full h-auto"
+              />
+            ) : (
+              <img
+                src={fotoPerfil}
+                alt="Imagen de perfil"
+                className="w-full h-auto"
+              />
+            )}
           </div>
         </div>
       </div>
